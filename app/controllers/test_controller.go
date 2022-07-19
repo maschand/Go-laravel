267a874ff3a198c4a209f535665e4d2cc40f78d8
candra17 <candra@digitels.com>
2022-07-19 16:51:42 +0700

    add routing templates and middleware

--- app/controllers/test_controller.go
+++ app/controllers/test_controller.go
@@ -42,6 +42,7 @@ func PostRequest(context *fiber.Ctx) error {
 	}
 
 	return context.Status(fiber.StatusCreated).JSON(fiber.Map{
+		"error":  false,
 		"result": body,
 	})
 }
@@ -73,12 +74,36 @@ func PutRequest(context *fiber.Ctx) error {
 	}
 
 	return context.Status(fiber.StatusOK).JSON(fiber.Map{
+		"error":  false,
 		"result": body,
 	})
 }
 
 func DeleteRequest(context *fiber.Ctx) error {
+	body := &models.Post{}
+
+	if err := context.BodyParser(body); err != nil {
+		// Return status 400 and error message.
+		return context.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": true,
+			"msg":   err.Error(),
+		})
+	}
+
+	// Create a new validator for a Book model.
+	validate := utils.NewValidator()
+
+	// Validate book fields.
+	if err := validate.Struct(body); err != nil {
+		// Return, if some fields are not valid.
+		return context.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": true,
+			"msg":   utils.ValidatorErrors(err),
+		})
+	}
+
 	return context.Status(fiber.StatusOK).JSON(fiber.Map{
-		"result": context.AllParams(),
+		"error":  false,
+		"result": "Successfull Delete Your Data",
 	})
 }
--- main.go
+++ main.go
@@ -2,13 +2,25 @@ package main
 
 import (
 	"github.com/create-go-app/fiber-go-template/app/controllers"
+	"github.com/create-go-app/fiber-go-template/pkg/middleware"
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
-	app := fiber.New()
+	//Initialization new app
+	app := fiber.New(fiber.Config{
+		Prefork:       true,
+		CaseSensitive: true,
+		StrictRouting: true,
+		ServerHeader:  "Midtrans",
+		AppName:       "Midtrans v1",
+	})
 
+	//Declare versioning or grouping
 	api := app.Group("/api")
+
+	//Declare Middleware
+	middleware.FiberMiddleware(app)
 
 	//Example Api CRUD
 	api.Get("/test", controllers.GetRequest)
