d1d5ea413e1f39056439324c385320ea4bb7068a
candra17 <candra@digitels.com>
2022-07-23 11:07:50 +0700

    add services

--- app/services/post_service.go
+++ app/services/post_service.go
@@ -1,1 +1,15 @@
 package services
+
+import (
+	"github.com/create-go-app/fiber-go-template/app/models"
+	"github.com/create-go-app/fiber-go-template/app/repositories"
+)
+
+func getPost() models.Post {
+	repo := repositories.NewPost()
+	return
+}
+
+//func (service PostService) getPost() models.Post {
+//	return models.Post{}
+//}
