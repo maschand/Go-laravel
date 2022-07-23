789e7a050a4b13a9ec8de8ecfdb690990725cb26
candra17 <candra@digitels.com>
2022-07-23 11:08:31 +0700

    add kernels

--- app/kernel.go
+++ app/kernel.go
@@ -1,1 +1,13 @@
-package app
+package kernels
+
+import (
+	"github.com/create-go-app/fiber-go-template/app/interfaces"
+)
+
+type Service struct {
+	Post interfaces.PostInterface
+}
+
+//func boot() {
+//	post := repositories.Post{}
+//}
