8311ed50dff6533c4ab6a45689c197d95cffbcd4
candra17 <candra@digitels.com>
2022-07-23 11:07:32 +0700

    add repositories

--- app/repositories/post_repository.go
+++ app/repositories/post_repository.go
@@ -1,1 +1,36 @@
 package repositories
+
+import (
+	"github.com/create-go-app/fiber-go-template/app/interfaces"
+	"github.com/create-go-app/fiber-go-template/app/models"
+)
+
+type Post struct {
+	PostRepository interfaces.PostRepository
+}
+
+func NewPost(postInterface interfaces.PostRepository) Post {
+	return Post{
+		PostRepository: postInterface,
+	}
+}
+
+func (repository Post) getPost() models.Post {
+	body := repository.getPost()
+	return body
+}
+
+func (repository Post) createPost() models.Post {
+	body := repository.createPost()
+	return body
+}
+
+func (repository Post) updatePost() models.Post {
+	body := models.Post{}
+	return body
+}
+
+func (repository Post) deletePost() models.Post {
+	body := models.Post{}
+	return body
+}
