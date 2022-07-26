2006649170482b2f5de550c423f7a739e7bd8988
candra17 <candra@digitels.com>
2022-07-26 21:14:24 +0700

    unit testing for database and migration

--- app/tests/database_test.go
+++ app/tests/database_test.go
@@ -1,11 +1,15 @@
 package tests
 
 import (
+	"fmt"
+	"github.com/chand19-af/digitels-template/app/models"
 	"github.com/chand19-af/digitels-template/platform/database"
 	"github.com/chand19-af/digitels-template/platform/migrations"
 	"github.com/joho/godotenv"
+	"gorm.io/gorm"
 	"log"
 	"testing"
+	"time"
 )
 
 func boot() {
@@ -17,21 +21,76 @@ func boot() {
 
 func TestConnection(t *testing.T) {
 	boot()
-	db, err := database.GormMysqlConnection()
-	if err != nil {
-		panic(err)
+	db := database.GormMysqlConnection()
+	if db.Error != nil {
+		panic(db.Error)
 	}
 	print(db)
-	print(err)
 }
 
 func TestRunMigration(t *testing.T) {
 	boot()
-	db, err := database.GormMysqlConnection()
-	if err != nil {
-		panic(err)
+	db := database.GormMysqlConnection()
+	if db.Error != nil {
+		panic(db.Error)
 	}
 	migrations.AutoMigration(*db)
 
 	print("migration success")
+}
+
+func TestCreate(t *testing.T) {
+	boot()
+	db := database.GormMysqlConnection()
+	if db.Error != nil {
+		panic(db.Error)
+	}
+	res := db.Create(&models.Hotel{
+		Model:    gorm.Model{},
+		Name:     "First Hotel",
+		CheckIn:  time.Time{},
+		CheckOut: time.Time{},
+		About:    "-",
+		Logo:     "-",
+		Status:   "0",
+		Timezone: "Makassar",
+	})
+
+	if res.Error != nil {
+		panic("cant create data!")
+	}
+
+	fmt.Println(res)
+}
+
+func TestUpdate(t *testing.T) {
+	boot()
+	db := database.GormMysqlConnection()
+	if db.Error != nil {
+		panic(db.Error)
+	}
+
+	res := db.First(&models.Hotel{
+		Model: gorm.Model{
+			ID: 1,
+		},
+	}).Updates(&models.Hotel{
+		Name: "Siti Kotijah",
+	})
+
+	if res.Error != nil {
+		panic("cant update data")
+	}
+}
+
+func TestGet(t *testing.T) {
+	boot()
+	db := database.GormMysqlConnection()
+	if db.Error != nil {
+		panic(db.Error)
+	}
+
+	res := db.First(&models.Hotel{}, "id = ?", 1)
+
+	fmt.Print(res.Get("*"))
 }
