8de868d0fb4a2e1dc915964eb0a7d54150718bd3
candra17 <candra@digitels.com>
2022-07-26 21:10:29 +0700

    Add Helper for response

--- app/helpers/helper.go
+++ app/helpers/helper.go
@@ -1,1 +1,11 @@
 package helpers
+
+import "github.com/gofiber/fiber/v2"
+
+func JsonResponse(context *fiber.Ctx, message string, code int, data interface{}, err error) error {
+	return context.Status(code).JSON(fiber.Map{
+		"result":  data,
+		"error":   err,
+		"message": message,
+	})
+}
