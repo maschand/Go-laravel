af9eb6c5bb1f12df3ee193b164a68b09bc1324be
candra17 <candra@digitels.com>
2022-07-26 20:29:14 +0700

    add service provider

--- app/providers/app_service_provider.go
+++ app/providers/app_service_provider.go
@@ -2,3 +2,39 @@
 // +build wireinject
 
 package providers
+
+import (
+	"github.com/chand19-af/digitels-template/app/repositories"
+	"github.com/chand19-af/digitels-template/app/services"
+	"github.com/google/wire"
+)
+
+var hotelService = wire.NewSet(
+	services.NewHotelServiceImpl,
+	wire.Bind(new(services.Hotel), new(*services.HotelServiceImpl)),
+)
+
+var hotelRepository = wire.NewSet(
+	repositories.NewHotelRepositoryImpl,
+	wire.Bind(new(repositories.Hotel), new(*repositories.HotelRepositoryImpl)),
+)
+
+func InitializedHotelRepository() *repositories.HotelRepository {
+	wire.Build(
+		hotelRepository,
+		repositories.NewHotelRepository,
+	)
+
+	return nil
+}
+
+func InitializedHotelService() *services.HotelService {
+	wire.Build(
+		hotelService,
+		hotelRepository,
+		repositories.NewHotelRepository,
+		services.NewHotelService,
+	)
+
+	return nil
+}
